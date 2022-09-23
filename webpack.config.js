66006fa7d3ed685615bd9a63d8467b99c70da479
constdre <lourdandre@gmail.com>
2022-09-23 19:00:48 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,119 @@
+const path = require('path')
+const webpack = require('webpack')
+const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin')
+const devMode = process.env.NODE_ENV !== "production"
+// -----------------------------------------------------
+// Set environment flag defaults
+// -----------------------------------------------------
+const webpackConfigDefaults = { mode: 'production' }
+
+// -----------------------------------------------------
+// Helper function for switching between dev and prod
+// -----------------------------------------------------
+function modeConfig(env) {
+    const configModes = {
+        development: {
+            devServer: {
+                compress: true,
+                hot: true,
+                static: [
+                    // Skip transpiling static files and access them directly.
+                    {
+                        directory: path.join(__dirname, './src/assets'),
+                        publicPath: '/assets'
+                    }
+                ],
+            },
+            devtool: 'source-map',
+            plugins:[
+                new HtmlWebpackPlugin({
+                    title:'React Development',
+                    template:'./src/index.html'
+                })
+            ]
+        },
+        production: {
+            devtool: 'source-map',
+            optimization: {
+                minimize: true,
+                minimizer: [new TerserPlugin()]
+            },
+            plugins: [
+                // Extract css into output folder
+                new MiniCssExtractPlugin(),
+                // Generate index.html in output and include css from MiniCssExtractPlugin
+                new HtmlWebpackPlugin({
+                    filename: './dist/index.html',
+                    title: 'Siwgood Suite',
+                    template: './src/index.html'
+                })
+            ]
+        }
+    };
+    return configModes[env.mode];
+}
+// -----------------------------------------------------
+// Helper function for all other configuration defaults
+// -----------------------------------------------------
+function defaultConfig(env) {
+    return {
+        mode: env.mode,
+        entry: {
+            app: './src/index.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|mjs)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                // css-loader: returns css file as a string
+                // style-loader: injects the css inside <head> in <style> blocks
+                {
+                    test: /\.css$/,
+                    // use: ['style-loader', 'css-loader']
+                    use: [
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.png/,
+                    //https://webpack.js.org/guides/asset-modules/
+                    type: 'asset/resource'
+                }
+            ]
+        },
+        // resolve: {
+        //     alias: aliasRegistry(__dirname)
+        // },
+        plugins: [
+            // Needed to ignore when using moment.js
+            new webpack.IgnorePlugin({
+                resourceRegExp: new RegExp(/^\.\/locale$/),
+                contextRegExp: /moment$/
+            }),
+        ],
+        performance: {
+            hints: false
+        }
+    };
+}
+
+// ---------------------------------------------------
+// Export the final merged configurations to webpack
+// ---------------------------------------------------
+module.exports = (env = webpackConfigDefaults) => {
+    return merge(defaultConfig(env), modeConfig(env));
+};
+

9086f4d094d1aa40c17fb6c6c4db7c438b7050d3
constdre <lourdandre@gmail.com>
2022-09-24 19:22:42 -0400

    fix: import App.js correctly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-import App from "./components/App";
+import App from "./app/App";
 
 window.onload = () => {
     const container = document.getElementById("react-root")

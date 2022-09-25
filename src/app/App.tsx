60a6f7865d7429eaf9506e8e902870118a33b951
constdre <lourdandre@gmail.com>
2022-09-25 17:00:54 -0400

    feat: create theme extension

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react'
 import Home from './home/Home';
+import theme from './theme';
+
 const App = () => (
     <>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <Home />
         </ChakraProvider>
     </>
---
+++ src/app/theme/index.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = {
+    styles:{
+        global:{
+            '#app-root':{
+                p:4
+            }
+        }
+    }
+}
+
+export default extendTheme(theme);
